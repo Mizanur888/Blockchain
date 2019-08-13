efee598556d7fc3549e2ccf29e1a5d6cd7adb525
Mizanur Rahman <rahmanm2@wit.edu>
2019-08-13 12:46:12 -0400

    About Page

--- lendmoney/src/component/UI/About.js
+++ lendmoney/src/component/UI/About.js
@@ -51,6 +51,83 @@ export default function About() {
         is possible through the use of aws Firebase NoSQL database. In detail,
         the frameworks and tools we expect to use are
       </div>
+      <div style={{ margin: "50px" }}>
+        <h3>Deliverables</h3>
+        <nav>
+          <ol>
+            <li style={{ backgroundColor: "#ccc" }}>Web app</li>
+
+            <p>
+              Login With wallet credentials After a successful loan, deposit the
+              money into the debtors account Smart contact that will hold the
+              contract conditions, between the Lender and Receiver After paying
+              off a loan, the money will move back to the lender
+            </p>
+
+            <li style={{ backgroundColor: "#ccc" }}>
+              Backend Set of APIs to interact with LendMoney smart contract
+            </li>
+            <p>
+              Transaction API ( Ability to create loans, pay loans, and populate
+              the front end)
+            </p>
+          </ol>
+          <ul style={{ backgroundColor: "#ccc" }}>
+            <li>Documentation </li>
+            <li>Proposal</li>
+            <li>Design</li>
+            <li>Presentation</li>
+            <li> Final report (This Repository) </li>
+          </ul>
+        </nav>
+      </div>
+      <div style={{ margin: "50px" }}>
+        <h2>Getting Started</h2>
+        <h5>Prerequisites to work with solidity</h5>
+        <p>
+          Ensure that the following is already installed and working on your
+          machine:
+        </p>
+        <ul style={{backgroundColor:"#ccc"}}>
+          <li>npm</li>
+          <li>truffle</li>
+          <li>ganache</li>
+          <li>node</li>
+          <li>ng</li>
+          <li>solidity (solc)</li>
+          <li>Web3.js * $ npm install web3@0.20.5</li>
+        </ul>
+        <h3>Install</h3>
+       <p> Download or fork the project from github. As long as you've installed everything listed above, you should be good to go!</p>
+      </div>
+      <div style={{ margin: "50px" }}>
+              <h3>Run</h3>
+              <ol style={{backgroundColor:"#ccc"}}>
+              <li>Start Ganache, keeping the same accounts by running './startserver' in a terminal. If bash scripts do not execute on your system you can manually execute the command from within the file.</li>
+             <li> In another terminal, navigate to the folder containing 'truffle-config.js', and run 'truffle migrate' to compile and deploy the contract.</li>
+             <li>Navigate back to the working directory (/Blockchain/lendMoney) and execute 'npm start'. The web page should automatically open.</li>
+             <li> Log in with an address and private key, and select the state (either Loaner or Debtor).</li>
+             <li>Start lending money!</li>
+              </ol>
+      </div>
+      <div style={{ margin: "50px" }}>
+      <h3>Features</h3>
+      <ul style={{backgroundColor:"#ccc"}}>
+      <li>As a debtor, you can create a new loan request.</li>
+      <li>Debtors can also view current and pending loans, and pay approved loans.</li>
+      <li>As a loaner, you can view pending and approved loans.</li>
+      <li>Loaners can also approve or reject pending loans.</li>
+      <li>Debtors can pay off loans</li>
+      </ul>
+      </div>
+      <div style={{ margin: "50px" }}>
+      <h2>Team members:</h2>
+       <ul style={{backgroundColor:"#ccc"}}> 
+       <li>Malnati Josh, Front end, Design, presentation</li>
+       <li> Affel Harrison, Front end, Solidity</li>
+       <li>Mizanur Raman, Front end, Backend</li>
+      </ul>
+      </div>
     </container>
   );
 }
